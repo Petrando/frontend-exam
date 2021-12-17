8f6c0411c756ade21200a1fcb08194d46474c295
Petrando <bangkitwisata@gmail.com>
2021-12-17 15:13:14 +0800

    feat:infinite scroll for tags page

--- pages/tags.js
+++ pages/tags.js
@@ -2,28 +2,40 @@ import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import Layout from "../components/layout";
 import TagCard from "../components/pages/tags/TagCard";
+import InfiniteScroll from 'react-infinite-scroll-component';
+import Loading from "../components/global/Loading"
 import styles from "../styles/components/pages/tags/Tags.module.css"
 
 const Tags = () => {
   const [tagsData, setData] = useState([])
+  const [page, setPage] = useState(1)  
+  const [isLoading, setLoading] = useState(true);
+  const [isMaxPage, setIsMax] = useState(false);
   const router = useRouter()
+  const {pageSize, keyword} = router
 
   useEffect(()=>{
-    //fetchData;
+    fetchData();
   }, [])
 
-  const fetchData = () => {
-    fetch("https://avl-frontend-exam.herokuapp.com/api/tags")
+  const fetchData = async () => {
+    await fetch(`https://avl-frontend-exam.herokuapp.com/api/tags?page=${page}&pageSize=${pageSize?pageSize:15}&keyword=${keyword?keyword:''}`)
       .then((res) => res.json())
       .then((json) => {
         console.log(json)
-        setData(tagsData.concat(json))
+        if(json.length > 0){
+          setData(tagsData.concat(json))
+        }
+        else{
+          setIsMax(true)
+        }
       })
       .catch(err => {
         console.log(err)
       })
+      setLoading(false)
+      setPage(page+1)
   }
-  console.log(router.query)
 
   return (
     <Layout>
@@ -31,12 +43,27 @@ const Tags = () => {
         <h3 className={styles.pageTitle}>Tags</h3>
         {
           tagsData.length > 0 &&
-          <div className={styles.tagsContainer}>
-          {
-            tagsData.map((tag, i)=><TagCard key={i} data={tag} />)
-          }
-          </div>
-        } 
+
+            <InfiniteScroll
+              className={styles.tagsContainer}
+              dataLength={tagsData.length} //This is important field to render the next data
+              next={fetchData}
+              hasMore={!isMaxPage}
+              loader={<Loading />}
+              endMessage={
+                <p style={{ textAlign: 'center' }}>
+                  <b>All tags loaded</b>
+                </p>
+              }
+            >
+            
+              {
+                tagsData.map((tag, i)=><TagCard key={i} data={tag} />)
+              }
+              
+            </InfiniteScroll>
+
+        }
       </div>
     </Layout>
   )
