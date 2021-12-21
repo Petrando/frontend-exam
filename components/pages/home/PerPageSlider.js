5de87908859c0fdd366e71acba547e2acc76f605
Petrando <bangkitwisata@gmail.com>
2021-12-21 11:43:55 +0800

    change homepage component div's into MuI Box

--- components/pages/home/PerPageSlider.js
+++ components/pages/home/PerPageSlider.js
@@ -1,16 +1,17 @@
+import {Box} from '@mui/material'
 import styles from "../../../styles/components/pages/home/PerPageSlider.module.css"
 import homeStyles from "../../../styles/components/pages/home/Home.module.css"
 import Slider from "../../global/Slider"
 
 const PerPageSlider = ({itemPerPage, setItemPerPage, count}) => {
   return (
-    <div className={styles.container}>
+    <Box className={styles.container}>
       <h4>{itemPerPage} Of Results Per Page</h4>
-      <div className={styles.resultCount}>
+      <Box className={styles.resultCount}>
         <span className={styles.countNumber}>{count}</span>{" "}<span className={styles.result}>results</span>
-      </div>
+      </Box>
       <Slider setItemPerPage={setItemPerPage} />
-    </div>
+    </Box>
   )
 }
 
--- components/pages/home/SearchInput.js
+++ components/pages/home/SearchInput.js
@@ -1,13 +1,13 @@
+import {Box} from '@mui/material'
 import TextInput from "../../global/TextInput"
 import styles from "../../../styles/components/pages/home/SearchInput.module.css"
-import homeStyles from "../../../styles/components/pages/home/Home.module.css"
 
 const SearchInput = ({searchText, setSearchText}) => {
   return (
-    <div className={styles.container}>
+    <Box className={styles.container}>
       <h4>Search</h4>
       <TextInput searchText={searchText} setSearchText={setSearchText} />
-    </div>
+    </Box>
   )
 }
 
