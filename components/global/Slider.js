419828ffc9a3a0e057654334e13246947d23c980
Petrando <bangkitwisata@gmail.com>
2021-12-22 17:12:18 +0800

    fine tuning slider label position at various screen sizes

--- components/global/Slider.js
+++ components/global/Slider.js
@@ -1,20 +1,44 @@
 import { useEffect, useState } from "react";
 import Slider from "rc-slider";
 import "rc-slider/assets/index.css";
-import { height } from "@mui/system";
-
-
+import useWindowDimensions from "../../hooks/useWindowDimensions";
 
 const MySlider = ({ setItemPerPage}) => {
+  const {width, height} = useWindowDimensions()
+
+  const [marginTop, setMarginTop] = useState(
+                                              width > 0 && width <= 414 ? {marginTop:'7px'} :
+                                              width > 414 && height > 640 ? {marginTop:'7px'} :
+                                              {}
+                                            );
   const [currentValue, setCurrentValue] = useState(5);
   const [perPageMarks, setPageMarks] = useState({
-    1: {style:{}, label:"3"},
-    2: {style:{}, label:"6"},
-    3: {style:{}, label:"9"},
-    4: {style:{}, label:"12"},
-    5: {style:{color:"#FFFFFF"}, label:"15"},
-    6: {style:{}, label:"50"}
+    1: {style:{ ...marginTop}, label:"3"},
+    2: {style:{ ...marginTop}, label:"6"},
+    3: {style:{ ...marginTop}, label:"9"},
+    4: {style:{ ...marginTop}, label:"12"},
+    5: {style:{ ...marginTop, color:"#FFFFFF"}, label:"15"},
+    6: {style:{ ...marginTop}, label:"50"}
   })
+
+  useEffect(()=>{
+    setMarginTop(
+      width > 0 && width <= 414 ? {marginTop:'7px'} :
+      width > 414 && height > 640 ? {marginTop:'7px'} :
+      {}
+    )
+  }, [width, height]);
+
+  useEffect(()=>{
+    let updatedMarks = {}
+    for(let prop in perPageMarks){
+      const style = perPageMarks[prop].style.hasOwnProperty('color')?{...marginTop, color:"#FFFFFF"}:
+                                                       {...marginTop} 
+      const label = perPageMarks[prop].label;
+      updatedMarks[prop] = {style, label}
+    }
+    setPageMarks(updatedMarks)
+  }, [marginTop])
 
   const changeValue = (newValue) => {
     setCurrentValue(newValue);
@@ -22,8 +46,8 @@ const MySlider = ({ setItemPerPage}) => {
     let updatedMarks = {}
     for(let prop in perPageMarks){
       
-      const style = parseInt(prop) === newValue?{color:"#FFFFFF"}:
-                                              { };
+      const style = parseInt(prop) === newValue?{...marginTop, color:"#FFFFFF"}:
+                                                {...marginTop };
       const label = perPageMarks[prop].label;
 
       updatedMarks[prop] = {style, label}
@@ -43,8 +67,7 @@ const MySlider = ({ setItemPerPage}) => {
       dotStyle={{opacity:0}}
       trackStyle={{borderRadius:'20px', height:'5px', backgroundImage:'linear-gradient(to right, #FF5C01, #FFD25F)'}}
       railStyle={{borderRadius:'20px', height:'5px'}}
-      handleStyle={{width:'20px', height:'20px', background:'#1B1B1B', border:'6px solid #FFD05D'}}
-      activeDotStyle={{color:'rgb(255, 0, 0)'}}
+      handleStyle={{width:'20px', height:'20px', background:'#1B1B1B', border:'6px solid #FFD05D'}}      
     />
   )
 }
