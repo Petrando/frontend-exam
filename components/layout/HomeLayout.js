989d866e445704284f4e56fe653c932e9f13cd4d
Petrando <bangkitwisata@gmail.com>
2021-12-22 15:28:47 +0800

    fix:fitting homepage mobile....

--- components/layout/HomeLayout.js
+++ components/layout/HomeLayout.js
@@ -1,17 +1,26 @@
 import {Box} from '@mui/material'
 import Navbar from "./Navbar";
 import WithFollowersLayout from "./WithFollowersLayout";
+import useWindowDimensions from "../../hooks/useWindowDimensions"
 import styles from "../../styles/components/layout/HomeLayout.module.css";
 
 const HomeLayout = ({children}) => {
+  const {height} = useWindowDimensions();
   return (
-    <Box className={styles.container}>      
+    <Box className={`${styles.container} ${height > 0 && "containerHeight"}`}>      
       <Navbar />
       <Box className={styles.main}>
         <WithFollowersLayout atHome={true}>
           {children}
         </WithFollowersLayout>
-      </Box>      
+      </Box>    
+      <style jsx>{`        
+        @media (max-width:414px){
+          .containerHeight {
+            max-height: ${height}px !important;
+          }
+        }
+      `}</style>  
     </Box>
   )
 }
