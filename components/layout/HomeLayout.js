ab1b705d84caaa9fc2559fb7b089b3937be6f6b3
Petrando <bangkitwisata@gmail.com>
2022-01-30 13:51:32 +0800

    fit height next js attempt

--- components/layout/HomeLayout.js
+++ components/layout/HomeLayout.js
@@ -21,9 +21,20 @@ const LayoutContent = ({children}) => {
   const {height} = useWindowDimensions();
 
   return (
-    <Box style={{width:"100vw", height:"100vh"}}>
+    <>
+    <div>
           <h3>Test main page</h3>
-        </Box>
+        </div>
+    <style global jsx>{`
+     html,
+      body,
+      body > div:first-child,
+      div#__next,
+      div#__next > div {
+        height: 100%;
+      }
+  `}</style>
+    </>
   )
 }
 
