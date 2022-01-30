979baf0c9f34a02583ecb77cbe67e4b8f28e7d1f
Petrando <bangkitwisata@gmail.com>
2022-01-30 15:09:33 +0800

    HomeLayout not relying on next js 100% height div hack

--- components/layout/HomeLayout.js
+++ components/layout/HomeLayout.js
@@ -1,7 +1,6 @@
 import {Box, ThemeProvider} from '@mui/material'
 import Navbar from "./Navbar";
 import WithFollowersLayout from "./WithFollowersLayout";
-import useWindowDimensions from "../../hooks/useWindowDimensions"
 import {useStyles} from "../../styles/components/layout/HomeLayout.module"
 import theme from "../../styles/theme"
 
@@ -18,54 +17,17 @@ const HomeLayout = ({children}) => {
 
 const LayoutContent = ({children}) => {
   const classes = useStyles()
-  const {height} = useWindowDimensions();
 
   return (
-    <>
-    <div>
-      <Box className={classes.container}>      
-        <Navbar />
-        <Box className={classes.main}>
-          <WithFollowersLayout atHome={true}>
-            {children}
-          </WithFollowersLayout>
-        </Box> 
-      </Box>
-    </div>
-    <style global jsx>{`
-     html,
-      body,
-      body > div:first-child,
-      div#__next,
-      div#__next > div {
-        height: 100%;
-        margin: 0px;
-        padding: 0px;
-      }
-  `}</style>
-    </>
+    <Box className={classes.container}>      
+      <Navbar />
+      <Box className={classes.main}>
+        <WithFollowersLayout atHome={true}>
+          {children}
+        </WithFollowersLayout>
+      </Box> 
+    </Box>
   )
 }
 
-/*
-<Box className={`${classes.container} containerHeight`}>      
-        <Box style={{width:"100vw", height:"100vh"}}>
-          <h3>Test main page</h3>
-        </Box>
-        <style jsx>{`        
-          @media (max-width:414px){
-            .containerHeight {
-              height:  ${height}px;
-              max-height: ${height}px !important;
-            }
-          }
-        `}</style>  
-    </Box>*/
-/*
-<Navbar />
-        <Box className={classes.main}>
-          <WithFollowersLayout atHome={true}>
-            {children}
-          </WithFollowersLayout>
-        </Box>    */
 export default HomeLayout;
